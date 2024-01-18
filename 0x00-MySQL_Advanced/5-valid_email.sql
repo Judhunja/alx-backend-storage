3ba4b0e0062c3aaae3446a51b8dc7b7e2f4505b9
Judhunja <judxhunja@gmail.com>
2024-01-18 23:10:26 +0300

    emmail validation mysql

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- creates a trigger that resets 'valid_email' only when the email has been changed
+DELIMITER $$
+CREATE TRIGGER reset_email BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email REGEXP '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,4}$' THEN        
+        SET NEW.valid_email = 1;
+    ELSE
+        SET NEW.valid_email = 0;
+    END IF;
+END;
+$$
