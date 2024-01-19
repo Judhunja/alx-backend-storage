0c63a86115be4a8cf0beefd8e16d578ed22a75c4
Judhunja <judxhunja@gmail.com>
2024-01-19 15:44:43 +0300

    changing duplicate variables to prevent conflicts

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,20 +1,14 @@
 -- stored procedure that computes and stores average score for a student
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN u_id INT)
 BEGIN
     DECLARE student_score FLOAT;
-    SELECT AVG(score) INTO student_score FROM corrections WHERE user_id = user_id;
+    SELECT AVG(score) INTO student_score FROM corrections WHERE user_id = u_id;
 
     IF student_score IS NULL THEN
         SET student_score = 0;
     END IF;
     -- update average_score for student if the student exists on the table
-    UPDATE users SET average_score = student_score WHERE id = user_id;
-
-    -- check if a row exists, if not insert the new row
-    IF ROW_COUNT() = 0 THEN
-        INSERT INTO users (id, average_score) VALUES (user_id, student_score);
-    -- alternative that can be used with the above line without the surrounding IF statement: ON DUPLICATE KEY UPDATE average_score = student_score;
-    END IF;
+    UPDATE users SET average_score = student_score WHERE id = u_id;
 END;
 $$
