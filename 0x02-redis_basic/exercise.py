4fe878e0558a332dc868f93b67dbd49630df95c3
Judhunja <judxhunja@gmail.com>
2024-01-25 23:03:51 +0300

    parametrize with correct conversion function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """This module contains a class Cache."""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -21,3 +21,35 @@ class Cache:
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable[[Union[str, bytes, int, float]], Union[str, bytes, float, int]] = None) -> Union[str, int, bytes, float]:
+        """
+        Take key as str and optional callable to
+        convert the key to a specific data type.
+        """
+        data = self._redis.get(key)
+        try:
+            return fn(data)
+        except Exception:
+            return data
+
+    def get_str(self, key: str):
+        """Return the value as a string."""
+        return self.get(key, lambda y: y.decode("utf-8"))
+
+    def get_int(self, key: str):
+        """Return the value as int."""
+        return self.get(key, int)
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
