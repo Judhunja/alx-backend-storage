6f3b357dfaf4b59f34190741623b6aaa9b760315
Judhunja <judxhunja@gmail.com>
2024-04-24 23:10:17 +0000

    fix: return inserted doc _id

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -3,5 +3,7 @@
 
 
 def insert_school(mongo_collection, **kwargs):
-    """ Inserts a new document in a collection based on kwargs """
-    return mongo_collection.insert_one(kwargs)
+    """ Inserts a new document in a collection based on kwargs
+        Return the inserted object id
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id
