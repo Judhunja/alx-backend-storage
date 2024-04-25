f7d1e459325caed52a59bc6a1a565af665c504f1
Judhunja <judxhunja@gmail.com>
2024-04-25 00:22:14 +0000

    fix: create non-existent files and update modifications

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,5 +3,7 @@
 
 
 def update_topics(mongo_collection, name, topics):
-    """ Changes all topics of a school document based on the name"""
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    """Changes all topics of a school document based on the name"""
+    mongo_collection.update_one(
+        {"name": name}, {"$set": {"topics": topics}}, upsert=True
+    )
