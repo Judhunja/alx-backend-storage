02bc5fe174bc960eff37873b5fa59bd78a39d56e
Judhunja <judxhunja@gmail.com>
2024-05-02 01:39:04 +0300

    conversion functions for get

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """This module contains a class Cache"""
 
 import redis
-from typing import Union
+from typing import Union, Callable
 from uuid import uuid4
 
 
@@ -21,3 +21,20 @@ class Cache:
         key = str(key)
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable[Union[str, bytes, int, float], Union[str, bytes, int, float]] = None) -> Union[str, bytes, int, float]:
+        """Returns data belonging to a key in the desired format or None if the data doesn't exist"""
+        data = self._redis.get(key)
+
+        try:
+            return fn(data)
+        except Exception:
+            return data
+
+    def get_str(self, key: str):
+        """Parametrizes get for strings"""
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str):
+        """Parametrizes get for ints"""
+        return self.get(key, int(key))
